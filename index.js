909259e015998b502a6ff2dad746648737d99d07
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2023-01-14 16:24:06 -0500

    Move all libs to repo

--- index.js
+++ index.js
@@ -7,33 +7,35 @@ const runBenchmark = require('./tool.js');
 
 function runBenchmarks (atomizer) {
   function read (file) {
-    let filePath = path.resolve(__dirname, file);
-    if (
-      file.includes('node_modules') &&
-      !fs.existsSync(path.resolve(__dirname, 'node_modules'))
-    ) {
-      filePath = path.resolve(__dirname, '..', '..', file);
+    const filePath = path.resolve(__dirname, 'libraries', file + '.css');
+    let data = '';
+
+    try {
+      data = String(fs.readFileSync(filePath));
+    } catch (error1) {
+      helpers.throwError('Error reading file', error1);
     }
-    return String(fs.readFileSync(filePath));
+
+    return data;
   }
 
   // Inputs
   const inputs = [
-    { library: 'Bootstrap',  version: 'v1.1.1',  file: './libraries/bootstrap-1.1.1.css'                    },
-    { library: 'Bootstrap',  version: 'v2.3.2',  file: './libraries/bootstrap-2.3.2.css'                    },
-    { library: 'Bootstrap',  version: 'v3.4.1',  file: './node_modules/bootstrap3/dist/css/bootstrap.css'   },
-    { library: 'Bootstrap',  version: 'v4.6.2',  file: './node_modules/bootstrap4/dist/css/bootstrap.css'   },
-    { library: 'Bootstrap',  version: 'v5.2.3',  file: './node_modules/bootstrap5/dist/css/bootstrap.css'   },
-    { library: 'Blue Steel', version: 'v0.3.0',  file: './libraries/bsds-0.3.0.css'                         },
-    { library: 'Bulma',      version: 'v0.9.4',  file: './libraries/bulma-0.9.4.css'                        },
-    { library: 'Foundation', version: 'v5.5.3',  file: './libraries/foundation-5.5.3.css'                   },
-    { library: 'Foundation', version: 'v6.7.5',  file: './node_modules/foundation6/dist/css/foundation.css' },
-    { library: 'Milligram',  version: 'v1.4.1',  file: './libraries/milligram-1.4.1.css'                    },
-    { library: 'Normalize',  version: 'v8.0.1',  file: './libraries/normalize-8.0.1.css'                    },
-    { library: 'Skeleton',   version: 'v2.0.4',  file: './libraries/skeleton-2.0.4.css'                     },
-    { library: 'Tailwind',   version: 'v2.2.19', file: './libraries/tailwind-2.2.19.css'                    },
-    { library: 'Water',      version: 'v2.1.1',  file: './libraries/water-2.1.1.css'                        },
-    { library: 'WWT.com',    version: '(2023)',  file: './libraries/wwt.css'                                }
+    { library: 'Bootstrap',  version: 'v1.1.1',  file: 'bootstrap-1.1.1'  },
+    { library: 'Bootstrap',  version: 'v2.3.2',  file: 'bootstrap-2.3.2'  },
+    { library: 'Bootstrap',  version: 'v3.4.1',  file: 'bootstrap-3.4.1'  },
+    { library: 'Bootstrap',  version: 'v4.6.2',  file: 'bootstrap-4.6.2'  },
+    { library: 'Bootstrap',  version: 'v5.2.3',  file: 'bootstrap-5.2.3'  },
+    { library: 'Blue Steel', version: 'v0.3.0',  file: 'bsds-0.3.0'       },
+    { library: 'Bulma',      version: 'v0.9.4',  file: 'bulma-0.9.4'      },
+    { library: 'Foundation', version: 'v5.5.3',  file: 'foundation-5.5.3' },
+    { library: 'Foundation', version: 'v6.7.5',  file: 'foundation-6.7.5' },
+    { library: 'Milligram',  version: 'v1.4.1',  file: 'milligram-1.4.1'  },
+    { library: 'Normalize',  version: 'v8.0.1',  file: 'normalize-8.0.1'  },
+    { library: 'Skeleton',   version: 'v2.0.4',  file: 'skeleton-2.0.4'   },
+    { library: 'Tailwind',   version: 'v2.2.19', file: 'tailwind-2.2.19'  },
+    { library: 'Water',      version: 'v2.1.1',  file: 'water-2.1.1'      },
+    { library: 'WWT.com',    version: '(2023)',  file: 'wwt'              }
   ];
 
   // Results
